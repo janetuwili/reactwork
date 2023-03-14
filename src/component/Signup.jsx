c61d69ff0cd59b77cd5904a4954ae8bb3f8ad4ab
janetuwili <janetuwili@gmail.com>
2023-03-14 00:22:56 -0700

    second push

--- src/component/Signup.jsx
+++ src/component/Signup.jsx
@@ -1,21 +1,63 @@
 import React, { Component } from 'react'
+import {Card,Input,Checkbox,Button,Typography,
+} from "@material-tailwind/react";
 
-export default class Signup extends Component {
+ class Signup extends Component {
   render() {
     return (
-      <div className='Signup'>
-        <form action="">
-            <label htmlFor="">name</label>
-            <input type="text" placeholder='enter your name' /><br />
-            <label htmlFor="">Email</label>
-            <input type="text" placeholder='enter your email' /><br />
-            <label htmlFor="">Password</label>
-            <input type="password" placeholder='enter password' /><br />
-            <label htmlFor="">ConfirmPassword</label>
-            <input type="text" placeholder='confirm password'/>
-            <button>Signup</button>
-        </form>
+      <div className='m-10'>
+        <Card color="transparent" shadow={false}>
+      <Typography variant="h4" color="blue-gray">
+        Sign Up
+      </Typography>
+      <Typography color="gray" className="mt-1 font-normal">
+        Enter your details to register.
+      </Typography>
+      <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+        <div className="mb-4 flex flex-col gap-6">
+          <Input size="lg" label="Name" />
+          <Input size="lg" label="Email" />
+          <Input type="password" size="lg" label="Password" />
+        </div>
+        <Checkbox
+          label={
+            (
+              <Typography
+                variant="small"
+                color="gray"
+                className="flex items-center font-normal"
+              >
+                I agree the
+                <a
+                  href="#"
+                  className="font-medium transition-colors hover:text-blue-500"
+                >
+                  &nbsp;Terms and Conditions
+                </a>
+              </Typography>
+            )
+          }
+          containerProps={{ className: "-ml-2.5" }}
+        />
+        <Button className="mt-6" fullWidth>
+          Register
+        </Button>
+        <Typography color="gray" className="mt-4 text-center font-normal">
+          Already have an account?{" "}
+          <a
+            href="#"
+            className="font-medium text-blue-500 transition-colors hover:text-blue-700"
+          >
+            Sign In
+          </a>
+        </Typography>
+      </form>
+    </Card>
+
+
+
       </div>
     )
   }
 }
+export default Signup
